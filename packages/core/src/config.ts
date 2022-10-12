1e396da1ac03ff18566ff742107a2254d1b232d7
Kamil Jakubus <kamiljkbz@protonmail.com>
2022-10-12 17:46:04 +0200

    change(core): reduce config methods

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -1,14 +1,12 @@
-import * as glob from "glob";
-import * as Path from "path";
-
 function decode(data = "") {
   return JSON.parse(Buffer.from(data, "base64").toString("utf8") || "{}");
 }
 
-const test = /^\/(.+\/)*(.+).config\.(.+)$/g;
-export function getNetworks(): Array<string> {
-  const paths = glob.sync(process.cwd() + "/networks/**.config.js");
-  return paths.map(path => [...(path.matchAll(test))][0][2]);
+export function getEnv(): any {
+  if (typeof process.env.__BUIDL3_ENV === "undefined")
+    throw "__BUIDL3_ENV variable needs to be present to run, exiting...";
+
+  return decode(process.env.__BUIDL3_ENV);
 }
 
 export function getNetworkEnv(): any {
