2b5744059f4db0467aa9d8bf9afd48c554ee16e1
Kamil Jakubus <kamiljkbz@protonmail.com>
2022-10-30 16:49:59 +0100

    change(cli): reorder cli entrypoint

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -4,16 +4,6 @@ import { cwd } from 'process';
 import { injectGlobal, injectNetwork } from "./env";
 
 async function run(argv) {
-  const cli = build()
-    .brand('buidl3')
-    .src(__dirname)
-    .plugins(cwd(), { matching: "cli" })
-    .plugins(cwd() + "/node_modules/@buidl3", { matching: "cli-*" })
-    .help()
-    .version()
-    .defaultCommand()
-    .create();
-
   const dev = argv[2] !== "--prod";
 
   injectGlobal(dev);
@@ -24,6 +14,16 @@ async function run(argv) {
   // Remove mode flag
   if (argv[2] === '--prod' || argv[2] === '--dev')
     argv.splice(2, 1)
+
+  const cli = build()
+    .brand('buidl3')
+    .src(__dirname)
+    .plugins(cwd(), { matching: "cli" })
+    .plugins(cwd() + "/node_modules/@buidl3", { matching: "cli-*" })
+    .help()
+    .version()
+    .defaultCommand()
+    .create();
 
   const toolbox = await cli.run(argv);
   return toolbox;
